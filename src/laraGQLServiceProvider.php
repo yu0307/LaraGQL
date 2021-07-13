ff49dffdf296dd6bcea883d06222230d7f70a5cc
Lucas <yu0307@gmail.com>
2021-07-12 21:28:01 -0700

    Init

---
+++ src/laraGQLServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace feiron\laragql;
+use Illuminate\Support\ServiceProvider;
+
+class laraGQLServiceProvider extends ServiceProvider{
+
+    public function boot(){
+
+    }
+
+    public function register(){
+              
+    }
+}
